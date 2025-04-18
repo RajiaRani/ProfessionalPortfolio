0901ca814986bedc53d13f0c6370aaf6a09dfa91
RajiaRani <Rajiarani.syal@gmail.com>
2025-04-18 11:18:32 -0500

    added

--- fronted/src/Navbar/Navbar.jsx
+++ fronted/src/Navbar/Navbar.jsx
@@ -8,64 +8,66 @@ import "./Navbar.css";
 import { useState } from "react";
 
 export default function Navbar() {
-const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-const toggleMenu = () => {
-  setIsMenuOpen(!isMenuOpen);
-};
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-const handleLinkClick = () => {
-  setIsMenuOpen(false); // This will close the menu after clicking
-};
+  const handleLinkClick = () => {
+    setIsMenuOpen(false); // This will close the menu after clicking
+  };
 
   return (
     <>
       <header>
-          <div className="navbar">
-            <div className="left-side">
-              <p><Link to="/">Chenchiah Mekalathuru</Link></p>
-            </div>
-            <div  className={`middle-side ${isMenuOpen ? "active" : ""}`}>
-              <ul className="nav-menu">
-                <li className="nav-item">
-                  <Link to="/about" className="nav-link" onClick={handleLinkClick}>About</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/projects" className="nav-link" onClick={handleLinkClick}>Projects</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/skills" className="nav-link" onClick={handleLinkClick}>Skills</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/education" className="nav-link" onClick={handleLinkClick}>Academic</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/experience" className="nav-link" onClick={handleLinkClick}>Experience</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/certificates" className="nav-link" onClick={handleLinkClick}>Certificates</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/contact" className="nav-link" onClick={handleLinkClick}>Contact</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/resume" className="nav-link" onClick={handleLinkClick}>Resume</Link>
-                </li>
-              </ul>
-              <div className="icon" onClick={toggleMenu}>
-              {!isMenuOpen && <FaBars />}
-            </div>
-            <div className="close" onClick={toggleMenu}>
-              {isMenuOpen && <MdDisabledByDefault />}
-            </div>
-            </div>
-
-            <div className="right-side">
-              <a href="https://www.linkedin.com/in/iamchenchu/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-              <a href="https://github.com/iamchenchu" target="_blank" rel="noopener noreferrer">  <FaGithub /></a>
-              <a href="https://medium.com/@chenchaiah.mekalathuru" target="_blank" rel="noopener noreferrer"><FaMedium /></a>
-            </div>
+        <div className="navbar">
+          <div className="left-side">
+            <p><Link to="/">Chenchiah Mekalathuru</Link></p>
           </div>
+          <div className={`middle-side ${isMenuOpen ? "active" : ""}`}>
+            <ul className="nav-menu">
+              <li className="nav-item">
+                <Link to="/about" className="nav-link" onClick={handleLinkClick}>About</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/projects" className="nav-link" onClick={handleLinkClick}>Projects</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/skills" className="nav-link" onClick={handleLinkClick}>Skills</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/education" className="nav-link" onClick={handleLinkClick}>Academic</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/experience" className="nav-link" onClick={handleLinkClick}>Experience</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/certificates" className="nav-link" onClick={handleLinkClick}>Certificates</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link" onClick={handleLinkClick}>Contact</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/resume" className="nav-link" onClick={handleLinkClick}>Resume</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="right-side">
+            <a href="https://www.linkedin.com/in/iamchenchu/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+            <a href="https://github.com/iamchenchu" target="_blank" rel="noopener noreferrer">  <FaGithub /></a>
+            <a href="https://medium.com/@chenchaiah.mekalathuru" target="_blank" rel="noopener noreferrer"><FaMedium /></a>
+          </div>
+
+          <div className="menu-toggle">
+            {!isMenuOpen ? (
+              <div className="icon" onClick={toggleMenu}><FaBars /></div>
+            ) : (
+              <div className="close" onClick={toggleMenu}><MdDisabledByDefault /></div>
+            )}
+          </div>
+        </div>
       </header>
     </>
   )
