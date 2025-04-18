63abfedac53d74c3e3b480133f5ecfd0da9f5bcd
RajiaRani <Rajiarani.syal@gmail.com>
2025-04-18 10:25:49 -0500

    added

--- fronted/src/Navbar/Navbar.jsx
+++ fronted/src/Navbar/Navbar.jsx
@@ -5,60 +5,69 @@ import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
 import { MdDisabledByDefault } from "react-icons/md";
 import "./Navbar.css";
-export default function Navbar(){
+import { useState } from "react";
 
-    return(
-        <>
+export default function Navbar() {
+const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+const toggleMenu = () => {
+  setIsMenuOpen(!isMenuOpen);
+};
+
+const handleLinkClick = () => {
+  setIsMenuOpen(false); // This will close the menu after clicking
+};
+
+  return (
+    <>
       <header>
-      <div className="main-navbar-container">
-            <div className="navbar">
-              <div className="left-side">
-                <p><Link to="/">Chenchiah Mekalathuru</Link></p>
-              </div>
-              <div className="middle-side">
+          <div className="navbar">
+            <div className="left-side">
+              <p><Link to="/">Chenchiah Mekalathuru</Link></p>
+            </div>
+            <div  className={`middle-side ${isMenuOpen ? "active" : ""}`}>
               <ul className="nav-menu">
                 <li className="nav-item">
-                  <Link to="/about" className="nav-link">About</Link>
-                  </li>
+                  <Link to="/about" className="nav-link" onClick={handleLinkClick}>About</Link>
+                </li>
                 <li className="nav-item">
-                  <Link to="/projects" className="nav-link">Projects</Link> 
-                  </li>
+                  <Link to="/projects" className="nav-link" onClick={handleLinkClick}>Projects</Link>
+                </li>
                 <li className="nav-item">
-                  <Link to="/skills" className="nav-link">Skills</Link>
-                  </li>
+                  <Link to="/skills" className="nav-link" onClick={handleLinkClick}>Skills</Link>
+                </li>
                 <li className="nav-item">
-                  <Link to="/education" className="nav-link">Academic</Link>
-                  </li>
+                  <Link to="/education" className="nav-link" onClick={handleLinkClick}>Academic</Link>
+                </li>
                 <li className="nav-item">
-                  <Link to="/experience" className="nav-link">Experience</Link>
-                  </li>
+                  <Link to="/experience" className="nav-link" onClick={handleLinkClick}>Experience</Link>
+                </li>
                 <li className="nav-item">
-                  <Link to="/certificates" className="nav-link">Certificates</Link>
-                  </li>
+                  <Link to="/certificates" className="nav-link" onClick={handleLinkClick}>Certificates</Link>
+                </li>
                 <li className="nav-item">
-                  <Link to="/contact" className="nav-link">Contact</Link>
-                  </li>
+                  <Link to="/contact" className="nav-link" onClick={handleLinkClick}>Contact</Link>
+                </li>
                 <li className="nav-item">
-                  <Link to="/resume" className="nav-link">Resume</Link>
-                  </li>
+                  <Link to="/resume" className="nav-link" onClick={handleLinkClick}>Resume</Link>
+                </li>
               </ul>
-              <div className="icon">
-              <FaBars />
-              </div>
-              <div className="close">
-              <MdDisabledByDefault />
-              </div>
-              </div>
-            
-              <div className="right-side">
-               <a href="https://www.linkedin.com/in/iamchenchu/"  target="_blank" rel="noopener noreferrer"><FaLinkedin/></a> 
-               <a href="https://github.com/iamchenchu"  target="_blank" rel="noopener noreferrer">  <FaGithub/></a>
-              <a href="https://medium.com/@chenchaiah.mekalathuru"  target="_blank" rel="noopener noreferrer"><FaMedium/></a>  
-              </div>
+              <div className="icon" onClick={toggleMenu}>
+              {!isMenuOpen && <FaBars />}
             </div>
-        </div>
+            <div className="close" onClick={toggleMenu}>
+              {isMenuOpen && <MdDisabledByDefault />}
+            </div>
+            </div>
+
+            <div className="right-side">
+              <a href="https://www.linkedin.com/in/iamchenchu/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+              <a href="https://github.com/iamchenchu" target="_blank" rel="noopener noreferrer">  <FaGithub /></a>
+              <a href="https://medium.com/@chenchaiah.mekalathuru" target="_blank" rel="noopener noreferrer"><FaMedium /></a>
+            </div>
+          </div>
       </header>
-        </>
-    )
+    </>
+  )
 
 }
